02a5f97950c40b41212c43d5bf0ef57cc5a13b73
seylatann <69602888+seylatann@users.noreply.github.com>
2020-09-19 14:38:37 -0700

    Week 5 House Assignment - rogue code.

---
+++ House_Project/House.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace House_Project
+{
+    class House
+    {
+
+        private int area;
+
+
+        public House(int aArea)
+        {
+            Area = aArea;
+        }
+
+        public int Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+
+    }
+
+    class Door
+    {
+        public string color;
+
+
+
+        public Door(string aColor)
+        {
+            color = aColor;
+
+        }
+
+        public void ShowData()
+        { Console.WriteLine("I'm a door that's colored " + color + "."); }
+
+      
+    }
+
+
+    
+
+    class Person
+    {
+        public string name;
+
+        public Person(string aName)
+        {
+            name = aName;
+        }
+    }
+}
---
+++ House_Project/Program.cs
@@ -0,0 +1,35 @@
+﻿ using System;
+
+namespace House_Project
+{
+
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            //practice using constructors, methods, and getters/setters.
+
+
+            House house1 = new House(166);
+            Console.WriteLine("I'm a house with an area of " + house1.Area + " m2.");
+
+         
+
+            Door door1 = new Door("blue");
+            door1.ShowData();
+
+
+            Person person1 = new Person("Seyla");
+            Console.WriteLine("My  name is  " + person1.name + " and I live here.");
+
+
+
+
+        }
+
+
+
+    }
+}
+
